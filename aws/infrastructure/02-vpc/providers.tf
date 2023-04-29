6121efdb789f48014ccd9c5432e8f5ef6f352928
Irfan Danish <irdanish11@gmail.com>
2023-04-29 21:52:35 +0500

    updated region and added backend configuration

--- aws/infrastructure/02-vpc/providers.tf
+++ aws/infrastructure/02-vpc/providers.tf
@@ -12,3 +12,15 @@ provider "aws" {
   region = "eu-west-2"
   profile = "aws-access-key-profile"
 }
+
+# Configuring S3 and DynamoDB backend for Terraform state
+# terraform init -reconfigure
+terraform {
+  backend "s3" {
+    bucket = "infrafy-backend-tfstate-bucket-dev"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
+    profile = "infrafy-tf-admin-dev"
+    dynamodb_table = "infrafy-tfstate-lock-dev"
+  }    
+}
